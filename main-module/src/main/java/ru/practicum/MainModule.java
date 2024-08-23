d5b7b44b7a89435abc28b18f16cb411f05b8c0b1
lazzzolvan <MR.Mersen@yandex.ru>
2024-08-23 20:10:59 +0300

    исправил под тесты

--- main-module/src/main/java/ru/practicum/MainModule.java
+++ main-module/src/main/java/ru/practicum/MainModule.java
@@ -2,10 +2,8 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ru.practicum", "ru.practicum"})
 public class MainModule {
     public static void main(String[] args) {
         SpringApplication.run(MainModule.class, args);
